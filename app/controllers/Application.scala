1e319347901ef7010d3de4434d01fba52f044013
rapptea <rapptea3@gmail.com>
2014-12-19 20:45:46 +0900

    refactor site structure and add some messages

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,8 +5,12 @@ import play.api.mvc._
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index(toolname: String) = Action {
+    toolname match { 
+      case "index" => Ok(views.html.index("Your new application is ready."))
+      case"roulette" => Ok(views.html.roulette("Roulette Tool"))
+      case _ => Ok(views.html.index("Your new application is ready."))
+    }
   }
 
 }
