41f4381247b01d960c5594ce28a7d74cd7889323
rapptea <rapptea3@gmail.com>
2014-12-22 05:36:32 +0900

    created stream page and website links

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,7 +8,8 @@ object Application extends Controller {
   def index(toolname: String) = Action {
     toolname match { 
       case "index" => Ok(views.html.index("Your new application is ready."))
-      case"roulette" => Ok(views.html.roulette("Roulette Tool"))
+      case "roulette" => Ok(views.html.roulette("Roulette Tool"))
+      case "lapack" => Ok(views.html.mystream("Lapack's Stream and chat page"))
       case _ => Ok(views.html.index("Your new application is ready."))
     }
   }
